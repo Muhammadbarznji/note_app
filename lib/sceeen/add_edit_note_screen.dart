f04b14dadbf73ca0e68871408fb9c39eb2bf861a
Muhammadbarznji <kimmmm702@gmail.com>
2022-01-24 16:59:38 +0300

    fix back dialog box

--- lib/sceeen/add_edit_note_screen.dart
+++ lib/sceeen/add_edit_note_screen.dart
@@ -30,59 +30,64 @@ class _AddEditNoteScreenState extends State<AddEditNoteScreen> {
     description = widget.note?.description ?? "";
   }
 
-/*  Future<bool?> dialog() {
-   return showDialog(
-      context: context,
-      builder: (BuildContext context) =>
-          AlertDialog(
-            title: const Text("Discard note"),
-            content: const Text("All Changes will be discarded"),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () =>  Navigator.pop(context, false),
-                child: const Text("Cancel"),
-              ),
-              TextButton(
-                onPressed: () =>
-                {
-                   true;
-                },
-                child: const Text("Discard"),
-              ),
-            ],
-          ) ?? false,
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: isChangeValue ? _onBackPressed : null,
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [saveButton()],
+        ),
+        body: Form(
+          key: _formKey,
+          child: NoteFormWidget(
+            isImportant: isImportant,
+            title: title,
+            description: description,
+            onChangedImportant: (isImportant) => setState(() {
+              this.isImportant = isImportant;
+              isChangeValue = true;
+            }),
+            onChangedTitle: (title) => setState(() {
+              this.title = title;
+              isChangeValue = true;
+            }),
+            onChangedDescription: (description) => setState(() {
+              this.description = description;
+              isChangeValue = true;
+            }),
+          ),
+        ),
+      ),
     );
-  }*/
+  }
 
-  Future<bool?> _showDialog(context) {
-    return showDialog(
+  Future<bool> _onBackPressed() async {
+    return await showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: const Text("Discard note"),
+            title: const Text(
+              "Discard note",
+              style: TextStyle(color: Colors.red),
+            ),
             content: const Text(
               "All Changes will be discarded",
-              style: TextStyle(color: Colors.red, fontSize: 20),
             ),
             actions: <Widget>[
-              FlatButton(
-                color: Colors.blue,
+              TextButton(
                 child: const Text(
                   "Cancel",
-                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  style: TextStyle(color: Colors.blue),
                 ),
                 onPressed: () {
                   Navigator.pop(context, false);
                 },
               ),
-              const SizedBox(
-                width: 50,
-              ),
-              FlatButton(
-                color: Colors.red,
+              TextButton(
                 child: const Text(
                   "Discard",
-                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  style: TextStyle(color: Colors.red),
                 ),
                 onPressed: () {
                   Navigator.pop(context, true);
@@ -91,60 +96,6 @@ class _AddEditNoteScreenState extends State<AddEditNoteScreen> {
             ],
           );
         });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            isChangeValue
-                ? showDialog(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      title: const Text("Discard note"),
-                      content: const Text("All Changes will be discarded"),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: const Text("Cancel"),
-                        ),
-                        TextButton(
-                          onPressed: () =>
-                              {Navigator.pop(context), Navigator.pop(context)},
-                          child: const Text("Discard"),
-                        ),
-                      ],
-                    ),
-                  )
-                : Navigator.pop(context);
-          },
-        ),
-        actions: [saveButton()],
-      ),
-      body: Form(
-        key: _formKey,
-        child: NoteFormWidget(
-          isImportant: isImportant,
-          title: title,
-          description: description,
-          onChangedImportant: (isImportant) => setState(() {
-            this.isImportant = isImportant;
-            isChangeValue = true;
-          }),
-          onChangedTitle: (title) => setState(() {
-            this.title = title;
-            isChangeValue = true;
-          }),
-          onChangedDescription: (description) => setState(() {
-            this.description = description;
-            isChangeValue = true;
-          }),
-        ),
-      ),
-    );
   }
 
   Widget saveButton() {
